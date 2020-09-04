0fe87bd66bac8be4b7ce0eb2959ad96b79034c2a
aapetsi <apetsiampiah@gmail.com>
2020-09-04 11:20:13 +0000

    add contact schema

--- models/contact.js
+++ models/contact.js
@@ -14,7 +14,28 @@ const contactSchema = new mongoose.Schema({
     required: true,
   },
   phone: {
+    type: Number,
+    required: true,
+  },
+  phone2: {
+    type: Number,
+  },
+  phone3: {
+    type: Number,
+  },
+  email: {
     type: String,
     required: true,
   },
+  email2: {
+    type: String,
+  },
+  email3: {
+    type: String,
+  },
+  twitter: {
+    type: String,
+  },
 })
+
+module.exports = mongoose.model('contact', contactSchema)
