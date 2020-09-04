8279ed1644c80362ad263e64ce2335a388a96ccd
aapetsi <apetsiampiah@gmail.com>
2020-09-04 03:47:10 +0000

    add author type

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const schema = require('./schema/schema')
 
 const app = express()
 
-app.use('/graphql', graphqlHTTP({ schema }))
+app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
 
 app.listen(4000, () => console.log(`Server running on port 4000`))
