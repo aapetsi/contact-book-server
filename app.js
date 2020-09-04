b90f3f7923747605c5d16c1d90a07fdfcbe1dc88
aapetsi <apetsiampiah@gmail.com>
2020-09-04 11:15:20 +0000

    begin add contact schema

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
 
 const app = express()
+
+mongoose
+  .connect('mongodb://localhost:27017/contacts', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => {
+    console.log('MongoDB Connected')
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }))
 
